80a17437ed1f0b06dda2f04549938f3deb028ef5
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-04-04 18:41:04 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,8 +7,8 @@ function setup() {
 }
 
 function draw() {
-  stroke(map(mouseX, 0,600,0, 255, true))
   if(mouseIsPressed == true){
+stroke(map(mouseX, 0,600,0, 255, true))  
     line( width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
   line(mouseX, mouseY, pmouseX, pmouseY);
   }
