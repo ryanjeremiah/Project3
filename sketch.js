316bb9d4fca3828aae5bce48a62dc2b8386f350d
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-04-04 18:47:50 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,4 +12,12 @@ stroke(map(mouseX, 0,600,0, 255, true))
     line( width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
   line(mouseX, mouseY, pmouseX, pmouseY);
   }
+function keyType(){
+  if(key === 'a'){
+    console.log('key a is being type')
+  }
+  
+  return false
+}
+
 }
