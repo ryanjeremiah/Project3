5708d2dbc89fdb86234a79208e91dd0c7c25137c
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-04-04 19:36:29 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,9 @@ function draw() {
   if(mouseIsPressed){  
      // line(mouseX, mouseY, pmouseX, pmouseY);
      array.push([mouseX, mouseY]);
+    
   }
+  
 function keyType(){
   
   if(key === 's'){
@@ -21,7 +23,12 @@ function keyType(){
   } else if (key==='d'){
     // display image
     
-    line(array[0][0], array[0][1], array[1][0], array[1][1]);
+    for(let i = 0; i< array.length; i++){
+      line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+    }
+     
+    // line(array[1][0], array[0][1], array[2][0], array[1][1]);
+    //line(array[0][0], array[0][1], array[1][0], array[1][1]);
   }
   
   return false
