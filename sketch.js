b62969ad2f6a5a517a17c7d22c8488c25afd527f
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-04-04 19:48:40 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,32 +1,39 @@
 let array = [];
 
-
 function setup() {
   createCanvas(1000, 1000);
   background(220);
+  
   strokeWeight(25);
+  noFill();
   
 }
 
-function draw() {
+function draw(){
+  
   if(mouseIsPressed){  
      // line(mouseX, mouseY, pmouseX, pmouseY);
+    background(0);
      array.push([mouseX, mouseY]);
+  }
     
   }
   
-function keyType(){
+function keyType() {
   
   if(key === 's'){
     // save this image
     saveCanvas('fileName', 'png');
   } else if (key==='d'){
     // display image
+    background(255);
     
+    beginShape();
     for(let i = 0; i< array.length; i++){
-      line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      curveVertex(array[i][0], array[i][1]);
     }
-     
+     endShape();
     // line(array[1][0], array[0][1], array[2][0], array[1][1]);
     //line(array[0][0], array[0][1], array[1][0], array[1][1]);
   }
