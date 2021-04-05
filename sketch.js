430b395a9fc18765c0b038b434d57c8a724594e5
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-04-04 19:18:47 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,17 @@
 let array = [];
+
+
 function setup() {
   createCanvas(600, 600);
   background(220);
-  
   strokeWeight(15);
   
 }
 
 function draw() {
-  if(mouseIsPressed == true){
-  stroke(map(mouseX, 0,600,0, 255, true))  
+  if(mouseIsPressed == true){  
      line(mouseX, mouseY, pmouseX, pmouseY);
-    // line( width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-    array.push([mouseX, mouseY]);
+     array.push([mouseX, mouseY]);
   }
 function keyType(){
   
