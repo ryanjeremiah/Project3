d49ff8a90c9b28376c72b0f5f7f2121651e44fe1
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-04-04 19:12:51 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,13 +9,17 @@ function setup() {
 function draw() {
   if(mouseIsPressed == true){
 stroke(map(mouseX, 0,600,0, 255, true))  
-    line( width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-  line(mouseX, mouseY, pmouseX, pmouseY);
+    // line( width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    // line(mouseX, mouseY, pmouseX, pmouseY);
   }
 function keyType(){
   
   if(key === 's'){
+    // save this image
     saveCanvas('fileName', 'png');
+  } else if (key === 'd'){
+    // display image
+    
   }
   
   return false
