3129ea8eec51260bbeeb9e409c056a4ef1354f67
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-04-11 21:21:24 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -39,12 +39,12 @@ function keyType() {
     
     beginShape();
     for(let i = 0; i< array.length; i++){
-      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
       curveVertex(array[i][0], array[i][1]);
     }
      endShape();
-    // line(array[1][0], array[0][1], array[2][0], array[1][1]);
-    //line(array[0][0], array[0][1], array[1][0], array[1][1]);
+    line(array[1][0], array[0][1], array[2][0], array[1][1]);
+    line(array[0][0], array[0][1], array[1][0], array[1][1]);
   }
   
   return false
