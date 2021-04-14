7d4609f8c6826b3d936e0dc6ddd7f01b89093125
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-04-14 16:16:08 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,37 @@
+let myImage;
+let c;
+
+function preload(){
+  myImage = loadImage('Jak_3.png') {   
+}
+  
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(52, 168, 235);
+  no
   
   strokeWeight(10);
+  
+}
+
+function line(){
+  background(Image);
+  c= myImage.get(mouseY, mouseX);
+  fill(c);
+  rectMode(Center);
+  rect(mouseX, mouseY, 45);
 }
 
 function draw(){
    if (mouseIsPressed == true){
      ellipse(width - mouseX, height - mouseY,  width - pmouseX,  height - pmouseY);
-     
    }
   
+function line(){
+  if (mouseIsPresse === false){
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY
+}
+
 function windowResized(){
   resizeCanvas(windowWidth, widowHeight);
 }
