fec3e2a3cf44dbcdf13a2ced4d8850fb76f119fd
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-04-13 22:55:17 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,11 @@
 let imgSave;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800,800);
   
   strokeWeight(20);
 }
 
 function draw(){
-  if (mouseIsPressed){
-    stroke(map(mouseX, 10, 540, 20, 32, true))
-    ellipse(width - mouseX, height - mouseY, width - pmouseX, height -pmouseY);
-    ellipse(mouseX, mouse, pmouse, pmouseY);  
-    
-    
-  }
-function windowResized(){
-resizeCanvas(widnowWidth, windowHeight);
-  background(3, 252, 252);
+   if (mouseIsPressed == true){
+     ellipse (mouseX, mouseY, 200,10);
 }
