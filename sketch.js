5ffb35020bb5416a273591be4a8aad04822d93f1
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-04-13 19:45:37 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,29 @@
+let value = 0;
 function setup() {
   createCanvas(800,800);
   background(400);
   
-  strokeWeight(20);
+  strokeWeight(10);
 }
 
 function draw(){
   if (!mouseIsPressed == false){
-    ellipse(mouseX, mouseY, pmouseX, pmouseY, 30,80);
+    stroke(map(mouseX, 1, 200, 2, 200, true))
+    ellipse(width - mouseX, height - mouseY, width - pmouseX, height -pmouseY);
+    ellipse(mouseX, mouse, pmouse, pmouseY);
   }
-}
 
+}
+ function keyTyped(){
+  console.log('key s{key} is being typed')
+  console.log("key" + key + "is being typed)
+           //save this image 
+  if (key === 's'){ 
+    console.log('key s is being type')
+  }
+   return false;
+ }
+ function save(){
+    saveCanvas(s,'fileName', 'jpg');
+ }
+}
