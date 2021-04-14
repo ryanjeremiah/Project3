c26ee23918e21fccf7a17c6d6d59b61e91821e7c
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-04-13 19:00:08 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,11 +2,12 @@ function setup() {
   createCanvas(800,800);
   background(400);
   
+  strokeWeight(20);
 }
 
 function draw(){
   if (!mouseIsPressed == false){
-    ellipse(mouseX, mouseY, 30,80);
+    ellipse(mouseX, mouseY, pmouseX, pmouseY, 30,80);
   }
 }
 
