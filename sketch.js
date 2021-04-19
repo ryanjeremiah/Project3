0b0b813cbb2bc4737474e7262887ed51de39cd0e
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-04-18 20:47:41 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,19 @@
  function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(73%, 45%, 0%, 4%);
+  background(#42f542);
   noCursor():
   
   strokeWeight(10);
-  
  }
+
+function windowResized(){
+  resizeCanvas(windowWidth, widowHeight); 
+}
 
 function draw(){
    if (keyIsPressed == true){
-     triangles():
-   }else if (key == 'r'){
-    ellipse():
-   }else if (key == 'j'){
-    rect();
+    if (key == 's'){
+    ellipse();
    }else if (key == 's'){
     saveCanvas('fileName', 'jpg');
    }
@@ -23,8 +23,19 @@ function mousePressed(){
   background(168)
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth, widowHeight);
-}
+function triangles(){
+ellipse (mouseX, mouseY, pmouseX, pmouseY);
+ translate(width, height);
+  ellipse((mouseX * -2), (mouseY * -4), (pmouseX * -6), (pmouseY * -8));
+  pop();
+  push();
+  translate(width, 0);
+  ellipse((mouseX * -4), (mouseY), (pmouseX * -8), (pmouseY));
+  pop();
+  push();
+  translate(0, height);
+  ellipse((mouseX), (mouseY * -3), (pmouseX), (pmouseY * -6));
+  pop();
+ 
  
 }
